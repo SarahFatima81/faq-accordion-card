f18584558fcfaba01e35b3c55c4fd4f550985295
Sarah Fatima <sfatima81@gmail.com>
2023-08-19 17:24:56 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import { useState } from "react";
+
+const faqs = [
+  {
+    question: "How many team members can I invite?",
+    answer:
+      "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.",
+  },
+  {
+    question: "What is the maximum file upload size?",
+    answer:
+      "No more than 2GB. All files in your account must fit your allotted storage space.",
+  },
+  {
+    question: "How do I reset my password?",
+    answer:
+      "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.",
+  },
+  {
+    question: "Can I cancel my subscription?",
+    answer:
+      "Yes! Send us a message and we’ll process your request no questions asked.",
+  },
+  {
+    question: "Do you provide additional support?",
+    answer:
+      "Chat and email support is available 24/7. Phone lines are open during normal business hours.",
+  },
+];
+function App() {
+  return (
+    <div className="accordion-card">
+      <div className="image-wrapper">
+        <div className="woman-online"></div>
+        <img
+          className="box"
+          src="\images\illustration-box-desktop.svg"
+          alt="box"
+        />
+      </div>
+      <div className="faqs">
+        <h1>FAQ</h1>
+        <Accordion data={faqs} />
+      </div>
+    </div>
+  );
+}
+function Accordion({ data }) {
+  return (
+    <div>
+      {data.map((el) => (
+        <AccordionItem question={el.question} answer={el.answer} />
+      ))}
+    </div>
+  );
+}
+function AccordionItem({ question, answer }) {
+  const [isOpen, setIsOpen] = useState(null);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+  return (
+    <div className="item">
+      <div className={`title-icon ${isOpen && "open"}`} onClick={handleToggle}>
+        <p className="red-active">{question}</p>
+        <img
+          className="toggle"
+          src="/images/icon-arrow-down.svg"
+          alt="toggle"
+        />
+      </div>
+      <div className="answer">{isOpen && <p>{answer}</p>}</div>
+      <hr></hr>
+    </div>
+  );
+}
+
+export default App;
